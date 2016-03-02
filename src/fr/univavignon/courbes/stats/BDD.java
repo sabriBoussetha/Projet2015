b2a66552da7acda69a6663849eadb4d21ec5f1d4
Brugvin Charlie <charlie.brugvin@gmail.com>
2016-03-02 13:31:54 +0100

    Create BDD.java

---
+++ src/fr/univavignon/courbes/stats/BDD.java
@@ -0,0 +1,22 @@
+package fr.univavignon.courbes.stats;
+
+/**
+ * fonctions qui permettent d'acceder a la BDD et de renvoyer les donnees
+ * 
+ * @author charlie
+ *
+ */
+public class BDD {
+	
+	/*
+	
+	public static int classementElo(int id_joueur)
+	
+	public static void majJoueur(int id_joueur);
+	
+	
+	*/
+	
+	
+
+}
