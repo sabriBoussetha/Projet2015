6039a6fc711ec2dff5b2fc7dd6cf60769c5802a9
Brugvin Charlie <charlie.brugvin@gmail.com>
2016-03-02 13:32:18 +0100

    Create Stats.java

---
+++ src/fr/univavignon/courbes/stats/Stats.java
@@ -0,0 +1,63 @@
+package fr.univavignon.courbes.stats;
+
+/**
+ * Classe contenant toutes les fonctions lie au traitement des donnes statistiques
+ * 
+ * @author charlie
+ *
+ */
+
+public class Stats {
+	
+	//ATTRIBUTS
+	
+	public static int D = 300;
+	public static int K = 30;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/*
+
+
+	//mettre a jour toute les stats sur la BDD, a l'issue d'une partie
+	//mettre a jour le classement ELO
+	//mettre a jour nb_partie faite, gaggne, ....
+	
+	
+	//mettre a jour le classement ELO de tout les joueurs d'une partie al'issue de celle ci
+	public static void majElo(Map<int, int> issue_partie)
+	{
+		//recuperer les ELO de tout les joueur sur la BDD
+		
+		//pour chaque joueur
+			
+			//determiner sa probabilité de succes moyenne
+			
+			//on incremente son classement avec :
+				//son ancien classement elo
+				//son coefficient de reussite
+				//sa probabilité de succee
+	}
+	
+	//calculer la probabilite moyenne de succes pour un joueur
+	public static double proba_moyenne(Map<int, int> elo, int id_joueur)
+	{
+		//pour tout les autres joueurs
+			//on calcule la probabilité de succe 1 vs 1
+		
+		//on fait la moyenne de ses sous-probabilité de succes
+		
+		//on la renvoie	
+	}
+	
+	//calcule du coefficient de reussite
+	public static double coef_reussite(int nb_joueur, int classement)
+	{
+		return (nb_joueur - classement) / ((nb_joueur*(nb_joueur-1))/2);
+	}
+	
+	*/
+}
